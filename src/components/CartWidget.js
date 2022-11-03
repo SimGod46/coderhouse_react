15a70e7ee00c20fbbaf534cb0827e0f3eef71ebb
simon <=sariasneira@gmail.com>
2022-11-02 23:22:06 -0300

    PreEntrega1+Arias

---
+++ src/components/CartWidget.js
@@ -0,0 +1,8 @@
+import cart from '../cart.png'
+function CartWidget() {
+  return (
+    <img src={cart} style={{width: "5vw"}}></img>
+  )
+}
+
+export default CartWidget
---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,7 @@
+function ItemListContainer(prop) {
+  return (
+    <div><p>{prop.greeting}</p></div>
+  )
+}
+
+export default ItemListContainer
