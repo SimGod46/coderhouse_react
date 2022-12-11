1a268ff946d066aa8f53905c6dc1f2e929d384c5
simon <=sariasneira@gmail.com>
2022-12-11 00:22:30 -0300

    Final c/ form dinamico

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,7 +1,12 @@
-import { useContext } from "react"
+import { addDoc, getFirestore, collection } from "firebase/firestore";
+import { useContext, useRef } from "react"
 import { cartContext } from "../../context/CartProvider"
 
 const Cart = () => {
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const telephoneRef = useRef();
+
   const contexto = useContext(cartContext);
   const dataList = contexto.cart;
   const finalTotal = dataList.reduce((prev,next) => Number(prev) + Number(next.precio)*Number(next.quantity),0);
@@ -20,6 +25,29 @@ const Cart = () => {
       </div>
     </div>);
   }
+  const createOrder = (email,name,telephone) =>{
+    const db = getFirestore();
+    const query = collection(db,"Order");
+    const newOrder = {
+      buyer:{
+        name:{name},
+        phone:{telephone},
+        email:{email}
+      },
+      items:contexto.cart,
+      total:finalTotal
+    };
+    addDoc(query,newOrder).then((response)=>{alert(`Orden creada con el id ${response.id}`)}).catch((error)=>console.log(error))
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const name = nameRef.current.value;
+    const telephone = telephoneRef.current.value;
+    createOrder(email,name,telephone);
+
+  };
 
   return (
     
@@ -36,24 +64,24 @@ const Cart = () => {
       </div>
 
       <div style={{display:"flex",justifyContent:"space-evenly", alignItems:"center",paddingTop:"10vh"}}>
-      <form style={{width:"60vw"}}>
+      <form style={{width:"60vw"}} onSubmit={handleSubmit}>
             <div className="form-group">
-              <label for="exampleInputEmail1">Dirección e-mail</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+              <label htmlFor="email">Dirección e-mail</label>
+              <input type="email" className="form-control" id="email" placeholder="Ingrese su email" ref={emailRef}/>
             </div>
 
             <div className="form-group">
-              <label for="exampleInputEmail1">Nombre</label>
-              <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+              <label htmlFor="nombre">Nombre</label>
+              <input type="text" className="form-control" id="nombre" placeholder="Ingrese su nombre" ref={nameRef}/>
             </div>
 
             <div className="form-group">
-              <label for="exampleInputEmail1">Telefono</label>
-              <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+              <label hrmlFor="telefono">Telefono</label>
+              <input type="text" className="form-control" id="telefono" placeholder="Ingrse su telefono" ref={telephoneRef}/>
             </div>
 
 
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary">ENVIAR</button>
           </form>
       </div>
 
